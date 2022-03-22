c82cc88e8463c3f262f6681ae389af537581b40c
Andreea Andrei Ioana <andreeaandrei971@yahoo.com>
2022-03-22 12:35:51 +0200

    I changed the names

--- src/ProjectForm.js
+++ src/ProjectForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import Header from './components/header';
-import FormComponent from './components/formComponent'
+import Header from './components/Header';
+import FormComponent from './components/FormComponent'
 import "./projectForm.css"
 import WishesComponent from './components/WishesComponent';
 
