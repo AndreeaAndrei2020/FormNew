cca2c20d61a4c57be7bd15ea79b11828e08803a4
Andreea Andrei Ioana <andreeaandrei971@yahoo.com>
2022-03-22 13:22:04 +0200

    save

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,17 @@
-import React  from 'react'
-import "../projectForm.css"
-export default function Header(props){
-  
-        return (
-            <>
-            <div id="amount">
-                   <p id="startedAmount"> Started amount:{props.startAmount}</p>
-                   <p id='totalAmount'> Total amount: {props.totalAmount}</p>
-                       
-                </div>
+import React from 'react'
 
-                <div id="nameAndData">@AndreiAndreea- 11/03/2022</div>
-            </>
-        )
-    
+function Header() {
+    return (
+        <div>
+            <div id="amount">
+                <p id="startedAmount"> Started amount:{props.startAmount}</p>
+                <p id='totalAmount'> Total amount: {props.totalAmount}</p>
+
+            </div>
+
+            <div id="nameAndData">@AndreiAndreea- 11/03/2022 </div>
+        </div>
+    )
 }
+
+export default Header
